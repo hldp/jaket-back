c05550812cf423c1009bef0df0adb3cbf86e7f3f
Lisa-Marie <ldemmer6@gmail.com>
2022-01-31 20:17:38 +0100

    Add swagger on index

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config({ path: process.env.DOTENV_CONFIG_PATH ?? '.env.prod' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+
+  const config = new DocumentBuilder()
+    .setTitle('Jaket API')
+    .setDescription('The jaket API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('', app, document);
+
+  await app.listen(process.env.PORT);
   new Logger('NestApplication').log(`Starting on PORT ${process.env.PORT}`);
 }
 bootstrap();

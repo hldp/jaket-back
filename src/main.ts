0631c56e65dfbbb6cfc17bab3fbbbe309d475d7c
Lisa-Marie <ldemmer6@gmail.com>
2022-01-31 20:37:32 +0100

    Last try

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document);
+  SwaggerModule.setup('/api', app, document);
 
   await app.listen(process.env.PORT);
   new Logger('NestApplication').log(`Starting on PORT ${process.env.PORT}`);

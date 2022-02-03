472e2fdbd783741a400bcdb582bd868278ad5a0d
damien-piedanna <da.piedanna@gmail.com>
2022-02-03 11:41:58 +0100

    port usage

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api', app, document);
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
   new Logger('NestApplication').log(`Starting on PORT ${process.env.PORT}`);
 }
 bootstrap();

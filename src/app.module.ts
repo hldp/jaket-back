071fbbd2f46dc58fbc0f69b673a4380c693c790d
Lisa-Marie <ldemmer6@gmail.com>
2022-02-07 16:30:20 +0100

    #8 : Add account

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,14 @@ import { Price, PriceSchema } from './schemas/price.schema';
 import { ScheduleModule } from '@nestjs/schedule';
 import { OpenDataService } from './import/opendata.service';
 import { AppController } from './app.controller';
+import { AccountController } from './account/account.controller';
+import { AccountService } from './account/account.service';
+import { User, UserSchema } from './schemas/user.schema';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './account/local.strategy';
+import { JwtStrategy } from './account/jwt.strategy';
+import * as bcrypt from 'bcrypt';
 
 dotenv.config({ path: process.env.DOTENV_CONFIG_PATH ?? './.env' });
 
@@ -21,10 +29,36 @@ dotenv.config({ path: process.env.DOTENV_CONFIG_PATH ?? './.env' });
       { name: Schedule.name, schema: ScheduleSchema },
       { name: Price.name, schema: PriceSchema },
     ]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          UserSchema.pre<User>('save', async function () {
+            const salt = await bcrypt.genSalt(10);
+            this.password = await bcrypt.hash(this.password, salt);
+          });
+          return schema;
+        },
+      },
+    ]),
     HttpModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '60s',
+      },
+    }),
     ScheduleModule.forRoot(),
   ],
-  controllers: [StationController, AppController],
-  providers: [StationService, OpenDataService],
+  controllers: [StationController, AccountController, AppController],
+  providers: [
+    StationService,
+    AccountService,
+    OpenDataService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
